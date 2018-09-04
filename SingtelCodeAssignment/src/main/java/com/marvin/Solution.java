8a57394436a79cae911c4af49473443b60610b8b
marvi <marvi@DESKTOP>
2018-09-04 22:03:06 +0800

    jUnit 4.12 was used to perform unit testing. To optimize maintainability, the existing design pattern was followed.

---
+++ SingtelCodeAssignment/src/main/java/com/marvin/Solution.java
@@ -0,0 +1,14 @@
+package com.marvin;
+
+import com.marvin.animals.Bird;
+
+public class Solution {
+
+	public static void main(String args[])
+	{
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+	}
+}
---
+++ SingtelCodeAssignment/src/main/java/com/marvin/animals/Bird.java
@@ -0,0 +1,13 @@
+package com.marvin.animals;
+
+public class Bird extends Animal {
+
+	public void fly()
+	{
+		System.out.println("I am flying");
+	}
+	
+	public void sing() {
+		System.out.print("I am singing");
+	}
+}
